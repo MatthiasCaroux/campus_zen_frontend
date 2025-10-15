0db00cbc0dc7d66185feba46652c0709fcb059e6
NiksanNagarajah <niksan.nagarajah@etu.univ-orleans.fr>
2025-10-15 19:21:45 +0200

    Ajustement pour utiliser url campuszenbackend-production

--- campusZen/services/AuthService.ts
+++ campusZen/services/AuthService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_URL = "https://campuszenbackend-production.up.railway.app/api/"; 
-const API_URL = "http://127.0.0.1:8000/api/"; 
+const API_URL = "https://campuszenbackend-production.up.railway.app/api/"; 
+// const API_URL = "http://127.0.0.1:8000/api/"; 
 
 export const register = async (emailPers: string, passwordPers: string) => {
   try {
