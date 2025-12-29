ba0b75675550149ffce3db502217e3a4840a4d23
MatthiasCaroux <lolmonyo@gmail.com>
2025-12-29 19:35:21 +0100

    maj de l'url de la BD

--- campusZen/config/endpoints.js
+++ campusZen/config/endpoints.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://campuszenbackend-prod.up.railway.app/api/';
+export const API_URL = 'http://54.38.35.105:8000/api/';
 // export const API_URL = 'http://127.0.0.1:8000/api/';
